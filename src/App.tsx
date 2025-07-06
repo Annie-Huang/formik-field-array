0ed4780826301b563132039cd00f8f5e9951ea37
Annie Huang <annie.hn@gmail.com>
2025-07-06 21:24:06 +1000

    Add <Formik> setup in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Card, CardContent } from '@mui/material';
+import { Form, Formik } from 'formik';
 
 function App() {
   return (
     <Card>
-      <CardContent></CardContent>
+      <CardContent>
+        <Formik initialValues={{}} onSubmit={() => {}}>
+          {({ values, errors }) => (
+            <Form>
+              <h1>Hello YouTube!</h1>
+
+              <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </CardContent>
     </Card>
   );
 }
