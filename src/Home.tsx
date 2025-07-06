ba0f4879bdcfd410a0895c5d583ccba13716d697
Annie Huang <annie.hn@gmail.com>
2025-07-06 21:33:31 +1000

    Move everything form App.tsx to Home.tsx

---
+++ src/Home.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, CardContent, Grid } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
+
+const INITIAL_FORM_STATE = {
+  fullName: '',
+  donationsAmount: 0,
+  termsAndConditions: false,
+};
+
+export const Home = () => {
+  return (
+    <Card>
+      <CardContent>
+        <Formik
+          initialValues={{
+            ...INITIAL_FORM_STATE,
+          }}
+          onSubmit={() => {}}
+        >
+          {({ values, errors }) => (
+            <Form>
+              <Grid container>
+                <Grid size={{ xs: 12 }}>
+                  <Field name='fullName' />
+                </Grid>
+              </Grid>
+
+              <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </CardContent>
+    </Card>
+  );
+};
