cfc05b2a4457e5d69fdcfebcb232654141018336
Annie Huang <annie.hn@gmail.com>
2025-07-06 21:42:28 +1000

    Create Toobar, resetCSS and create theme.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import { Home } from './Home';
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+import { theme } from './theme';
 
 function App() {
-  return <Home />;
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <AppBar position='fixed'>
+          <Toolbar variant='dense'>
+            <Typography variant='h6'>Field Array Formik</Typography>
+          </Toolbar>
+        </AppBar>
+
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Container>
+          <Box marginTop={10}>
+            <Home />
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
