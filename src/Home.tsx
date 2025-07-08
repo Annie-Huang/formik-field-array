c03d0117af82491ac3f6df70231305b1c252993c
Annie Huang <annie.hn@gmail.com>
2025-07-08 21:56:52 +1000

    Create the first item in the donation and create the FieldArray component in the template

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ import {
   CircularProgress,
   Grid,
 } from '@mui/material';
-import { Field, Form, Formik } from 'formik';
+import { Field, FieldArray, Form, Formik } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-mui';
 import * as Yup from 'yup';
 import { boolean, number, string } from 'yup';
@@ -16,7 +16,7 @@ const INITIAL_FORM_STATE = {
   fullName: '',
   donationsAmount: 0,
   termsAndConditions: false,
-  donations: [],
+  donations: [{ institution: '', percentage: 0 }],
 };
 
 const FORM_VALIDATION = Yup.object().shape({
@@ -73,10 +73,16 @@ export const Home = () => {
                   {/*<Field name='donations[1]' />*/}
                   {/*<Field name='donations[2]' />*/}
 
-                  <Field name='donations.0.institution' />
-                  <Field name='donations[0].percentage' />
-                  <Field name='donations[1].institution' />
-                  <Field name='donations[1].percentage' />
+                  {/*<Field name='donations.0.institution' />*/}
+                  {/*<Field name='donations[0].percentage' />*/}
+                  {/*<Field name='donations[1].institution' />*/}
+                  {/*<Field name='donations[1].percentage' />*/}
+
+                  <FieldArray name='donations'>
+                    {({ push, remove }) => (
+
+                    )}
+                  </FieldArray>
 
                   <Grid size={{ xs: 12 }}>
                     <Field
